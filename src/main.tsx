e326dd47f8da1ef88783ccf8dfc8a9e415f07f89
gjonika <gjonika@yahoo.com>
2025-05-17 22:10:31 +0300

    re

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import IndexPage from './pages/Index';
+import Dashboard from './pages/Dashboard';
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<IndexPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
